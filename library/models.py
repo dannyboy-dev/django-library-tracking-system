c3403575650172c541c842df8ca8c054b1bef69a
Dansol Obondo <dansolobondo7@gmail.com>
2026-02-13 17:10:44 +0300

    Interview

--- library/models.py
+++ library/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
@@ -42,5 +43,14 @@ class Loan(models.Model):
     return_date = models.DateField(null=True, blank=True)
     is_returned = models.BooleanField(default=False)
 
+    due_date  = models.DateField(auto_now_add=True)
+
     def __str__(self):
         return f"{self.book.title} loaned to {self.member.user.username}"
+    
+    def save(self, *args, **kwrags):
+
+        if not self.due_date:
+            self.due_date = self.due_date + timezone.timedelta(days=14)
+
+        super().save(*args, **kwrags)
